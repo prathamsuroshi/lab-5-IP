c66b0e0c31e41f8a2b67e047b8c3ebdfa820acc6
prathamsuroshi <psuroshi9@gmail.com>
2022-10-29 12:23:52 +0530

    last commit

--- src/components/Myskills.js
+++ src/components/Myskills.js
@@ -1,20 +1,24 @@
 import React from "react"
 import "./Myskills.css"
 
-function Myskills(props) {
-    return(
-        <div className="container">
-            <div className="skills">
-                    <div className="myskils">
-                        <h3 className="edu">My skills</h3>
-                        <li className="edu">JAVA program</li>
-                        <li className="edu">Python program</li>
-                        <li className="edu">c-programming</li>
-                        <li className="edu">Html/css</li>
+class Myskills extends React.Component {
+    render(){
+        return(
+            <div className="container">
+                <div className="skills">
+                        <div className="myskils">
+                            <h3 className="edu">My skills</h3>
+                            <li className="edu">JAVA program</li>
+                            <li className="edu">Python program</li>
+                            <li className="edu">c-programming</li>
+                            <li className="edu">Html/css</li>
+                        </div>
                     </div>
-                </div>
-        </div>
-    )
+            </div>
+        )
+
+    }
+    
 }
 
 export default Myskills
